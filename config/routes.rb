36db0bfcb2f787a5a9ef43a072d7fe904601d7a2
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-04-03 10:42:46 +0100

    Add default route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   get '/', :to => redirect('/static/index.html')
+
+  root to: "home#index"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
