f9352f3e3b3b5c5004782af813cd6fcbf9643535
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-04-13 07:19:31 +0100

    Add click Login scenario

--- spec/features/sign_up_feature_spec.rb
+++ spec/features/sign_up_feature_spec.rb
@@ -1,18 +1,25 @@
 require "rails_helper"
 
-feature "user creates account" do
+feature "User creates account" do
+  @javascript
   scenario "with valid data" do
-  	visit new_user_registration_path
-  		fill_in 'user_name', with: 'kingsley'
-	  	fill_in 'user_email', with: 'kingsley@test.com'
-	  	fill_in 'user_password', with: 'thisisatestpassword'
-	  	fill_in 'user_password_confirmation', with: 'thisisatestpassword'
-	  	click_button 'sign_up'
-
+  	  visit new_user_registration_path
+      
+      within "#new_user" do
+    		fill_in 'user_name', with: 'kingsley'
+  	  	fill_in 'user_email', with: 'kingsley@test.com'
+  	  	fill_in 'user_password', with: 'thisisthetestsubmit'
+  	  	fill_in 'user_password_confirmation', with: 'thisisthetestsubmit'
+      end
+      click_button 'Sign up'
+      
 	 	 	expect(current_path).to eq(root_path)
+      expect(page).to have_content('Welcome! You have signed up successfully.')
   end
 
-  scenario "with invalid data" do
-
+  scenario 'click Login in' do 
+    visit new_user_registration_path
+    click_link 'Log in'
+    expect(current_path).to eq(new_user_session_path)
   end
 end
