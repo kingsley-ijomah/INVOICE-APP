02a3319f9614bdd0fda1bebcd070a2e9011df612
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-04-15 16:28:26 +0100

    Use plain layout

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,8 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-before_filter :configure_sign_up_params, only: [:create]
-before_filter :configure_account_update_params, only: [:update]
+  before_filter :configure_sign_up_params, only: [:create]
+  before_filter :configure_account_update_params, only: [:update]
+
+  layout 'plain'
 
   # GET /resource/sign_up
   # def new
