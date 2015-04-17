00bed205c8fc6f70e1f4838729e750b89fb9f3f1
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-04-17 13:16:30 +0100

    Add create method

--- app/controllers/organisations_controller.rb
+++ app/controllers/organisations_controller.rb
@@ -2,4 +2,19 @@ class OrganisationsController < ApplicationController
 	def new
 		@organisation = Organisation.new
 	end
+
+	def create
+		@organisation = Organisation.new(organisation_params)
+		if @organisation.save
+			redirect_to 'index'
+		else
+			render 'new'
+		end
+	end
+
+	private
+
+	def organisation_params
+		params.require(:organisation).permit(:name, :street_1, :street_2, :city, :town, :post_code)
+	end
 end
