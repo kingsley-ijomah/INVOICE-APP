4c17c7a36deab4c7e97e3a8379c4c36775d21ffb
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-04-17 13:18:23 +0100

    Add factory for organisation

---
+++ spec/factories/organisations.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :organisation do
+	  name Faker::Company.name
+		street_1 Faker::Address.street_name
+		street_2 Faker::Address.secondary_address
+		city Faker::Address.city
+		town Faker::Address.state
+		post_code Faker::Address.postcode
+  end
+end
