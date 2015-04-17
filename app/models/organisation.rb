72b1a6d1ae9f1a8844e75ce975465c2f74cc727b
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-04-17 13:16:48 +0100

    Add validations

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -1,4 +1,16 @@
-class Organisation 
-	include Mongoid::Document
+class Organisation
+  include Mongoid::Document
+  include Mongoid::Timestamps
 
+  validates :name, presence: true
+  validates :street_1, presence: true
+  validates :city, presence: true
+  validates :town, presence: true
+
+  field :name, type: String
+  field :street_1, type: String
+  field :street_2, type: String
+  field :city, type: String
+  field :town, type: String
+  field :post_code, type: String
 end
