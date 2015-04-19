016e3fad4cee4295e1f6ae366571b23a7bd37773
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-04-19 12:40:00 +0100

    Create client model

---
+++ app/models/client.rb
@@ -0,0 +1,13 @@
+class Client
+	include Mongoid::Document
+	include Mongoid::Timestamps
+
+	belongs_to :organisation
+
+	field :conatct_type, type: Boolean
+	field :first_name, type: String
+	field :last_name, type: String
+	field :email, type: String
+	field :tel, type: Integer
+	field :note, type: String
+end
