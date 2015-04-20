7be4fae9c6da291fae0aabeaf3e5f0825258ae4e
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-04-20 14:17:47 +0100

    Test reading all clients

--- spec/controllers/clients_controller_spec.rb
+++ spec/controllers/clients_controller_spec.rb
@@ -2,34 +2,41 @@ require 'rails_helper'
 include SharedMockSpecHelper
 
 describe ClientsController do 
-	describe 'valid client' do 
-		it 'Get #new' do  
+	describe 'Get #index' do
+		it 'assigns all clients to variable @client' do
+			get 'index'
+			expect(assigns[:clients]).not_to be_nil 
+		end
+	end
+
+	describe 'Get #new' do 
+		it 'assigns instance of client to @client' do  
 			get 'new'
 			expect(assigns[:client]).to be_an_instance_of Client
 		end
+	end
 
-		describe 'Post #create' do 
-			let(:double_client) { double('client') }
+	describe 'Post #create' do 
+		let(:double_client) { double('client') }
 
-			it 'valid post' do
-				attributes = attributes_for(:client)
-				stub_new_save(model: Client, attributes: attributes, double: double_client, return_value: true)
+		it 'valid post' do
+			attributes = attributes_for(:client)
+			stub_new_save(model: Client, attributes: attributes, double: double_client, return_value: true)
 
-				post 'create', client: attributes
+			post 'create', client: attributes
 
-				expect(response).to redirect_to clients_path
-				spy_new_save(model: Client, attributes: attributes, double: double_client) 
-			end
+			expect(response).to redirect_to clients_path
+			spy_new_save(model: Client, attributes: attributes, double: double_client) 
+		end
 
-			it 'invalid post' do
-				attributes = attributes_for(:client, first_name: nil)
-				stub_new_save(model: Client, attributes: attributes, double: double_client, return_value: false)
+		it 'invalid post' do
+			attributes = attributes_for(:client, first_name: nil)
+			stub_new_save(model: Client, attributes: attributes, double: double_client, return_value: false)
 
-				post 'create', client: attributes
+			post 'create', client: attributes
 
-				expect(response).to render_template 'new'
-				spy_new_save(model: Client, attributes: attributes, double: double_client) 
-			end
+			expect(response).to render_template 'new'
+			spy_new_save(model: Client, attributes: attributes, double: double_client) 
 		end
 	end
 end
