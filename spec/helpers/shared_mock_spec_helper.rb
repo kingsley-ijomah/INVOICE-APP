97ca28fce5e33c14d2c8d84cb2d3bc3f05f286fb
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-04-21 13:14:26 +0100

    Extract stub and mock methods

--- spec/helpers/shared_mock_spec_helper.rb
+++ spec/helpers/shared_mock_spec_helper.rb
@@ -8,4 +8,14 @@ module SharedMockSpecHelper
 		expect(model).to have_received(:new).with(attributes)
 		expect(double).to have_received(:save)
 	end
+
+	def stub_find_update(model:, id:, attributes:, double:, return_value:)
+		allow(model).to receive(:find).with(id).and_return(double)
+		allow(double).to receive(:update).with(attributes).and_return(return_value)
+	end
+
+	def spy_find_update(model:, id:, attributes:, double:)
+		expect(Client).to have_received(:find).with(id)
+		expect(double).to have_received(:update).with(attributes)
+	end
 end
