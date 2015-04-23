b2e6c1474a4757868517c21c4451165761e34426
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-04-23 09:57:40 +0100

    Syntax fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-	def showValidationErrors(model:)
+	def showValidationErrors model:
 		if model.errors.full_messages.any?
 			render :partial => "shared/validation_errors", :locals => {:messages => model.errors.full_messages}
     end
