cfe08aa70c0572d7c2160a5ca626de6f027bcd21
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-04-24 13:46:27 +0100

    Test feature for invoice valid and invalid post

--- spec/features/invoice_feature_spec.rb
+++ spec/features/invoice_feature_spec.rb
@@ -1,15 +1,24 @@
 require 'rails_helper'
 
 feature Invoice do
+	scenario 'visit invoice #new' do 
+		visit invoices_path
+		click_link 'Create Invoice'
+		expect(current_path).to eq new_invoice_path
+	end
+
 	scenario 'valid invoice' do 
 		client = create(:client)
 
 		visit new_invoice_path
-		select client.first_name, from: 'invoice_client_id'
-		fill_in 'invoice_number', with: '11'
-		fill_in 'invoice_date_of_issue', with: Time.now
-		fill_in 'invoice_po_number', with: '1'
-		fill_in 'invoice_discount', with: '10'
-		fill_in 'invoice_note', with: 'thank you for your business'
+		expect {
+			select client.first_name, from: 'invoice_client_id'
+			fill_in 'invoice_number', with: '11'
+			fill_in 'invoice_date_of_issue', with: Time.now
+			fill_in 'invoice_po_number', with: '1'
+			fill_in 'invoice_discount', with: '10'
+			fill_in 'invoice_note', with: 'thank you for your business'
+			click_button 'Save Draft'
+		}.to change(Invoice, :count).by(1)
 	end
 end
