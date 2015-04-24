8851a096c137686dcacd9ae227ac5594eaeb7d39
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-04-24 13:45:14 +0100

    Add create method for invoice

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -5,4 +5,19 @@ class InvoicesController < ApplicationController
   def new
   	@invoice = Invoice.new
   end
+
+  def create
+  	@invoice = Invoice.new(invoice_params)
+  	if @invoice.save
+  		redirect_to invoices_path
+  	else
+  		render 'new'
+  	end
+  end
+
+  private
+
+  def invoice_params
+  	params.require(:invoice).permit(:client_id, :organisation_id, :number, :date_of_issue, :po_number, :discount, :note)
+  end
 end
