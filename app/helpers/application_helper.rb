103f3498012e903b6babde61519cf0d064d9ec77
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-04-24 10:41:52 +0100

    Remove validation helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,3 @@
 module ApplicationHelper
-	def showValidationErrors model:
-		if model.errors.full_messages.any?
-			render :partial => "shared/validation_errors", :locals => {:messages => model.errors.full_messages}
-    end
-	end
+
 end
