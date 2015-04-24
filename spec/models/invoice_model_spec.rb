bc3942cbfebcda4c1d8db2aa221c3e6f7c561f17
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-04-24 09:45:50 +0100

    Adding model spec for invoice

---
+++ spec/models/invoice_model_spec.rb
@@ -0,0 +1,17 @@
+	require 'rails_helper'
+
+describe Invoice do 
+	describe 'relations' do
+		it { is_expected.to belong_to :organisation }
+		it { is_expected.to belong_to :client }
+	end
+
+	describe 'fields' do
+		it { is_expected.to have_fields(:number, :date_of_issue, :po_number, :discount, :note)}
+	end
+
+	describe 'validation' do 
+  	it { should validate_presence_of :number }
+  	it { should validate_presence_of :date_of_issue }
+  end
+end
