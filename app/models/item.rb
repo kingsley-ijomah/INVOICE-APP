1e9e8eff07ba7574eed44ffc1f27b943b6a160e9
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-04-29 14:12:24 +0100

    Add fields and relationships

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,12 @@ class Item
 	include Mongoid::Document
 	include Mongoid::Timestamps
 
+	belongs_to :invoice
+	belongs_to :item_kind
+
+	field :qty, type: Integer
+	field :kind, type: String
+	field :description, type: String
+	field :price, type: String
+	field :total, type: String
 end
