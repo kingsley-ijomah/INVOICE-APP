fcd0e8e9a3a4fe07e6ba790ed6b012d7d9fef481
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-05-02 22:03:13 +0100

    Updating spec feature

--- spec/features/invoice_feature_spec.rb
+++ spec/features/invoice_feature_spec.rb
@@ -9,9 +9,10 @@ feature Invoice do
 
 	scenario 'valid invoice' do
 		client = create(:client)
+		item_kind = create(:item_kind)
 
 		visit new_invoice_path
-		find("#add_line").click
+		# find("#add_line").click
 
 		expect {
 			select client.first_name, from: 'invoice_client_id'
@@ -21,10 +22,10 @@ feature Invoice do
 			fill_in 'invoice_discount', with: '10'
 			fill_in 'invoice_note', with: 'thank you for your business'
 
-			fill_in 'item_qty', with: 3
-			select 'service', from: 'item_item_kind_id'
-			fill_in 'item_description', with: 'A month of coding'
-			fill_in 'item_price', width: 3000
+			fill_in 'invoice_items_qty', with: '3'
+			select item_kind.name, from: 'invoice_items_item_kind_id'
+			fill_in 'invoice_items_description', with: 'A month of coding'
+			fill_in 'invoice_items_price', with: 3000
 
 			click_button 'Save Draft'
 		}.to change(Invoice, :count).by(1)
