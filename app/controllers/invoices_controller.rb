e5fee3398efcb9a65499f8ff67c55b04b0ddfb3a
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-05-03 09:54:08 +0100

    Add support for nested attributes items

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -8,6 +8,7 @@ class InvoicesController < ApplicationController
 
   def create
   	@invoice = Invoice.new(invoice_params)
+    
   	if @invoice.save
   		redirect_to invoices_path
   	else
@@ -21,6 +22,14 @@ class InvoicesController < ApplicationController
   private
 
   def invoice_params
-  	params.require(:invoice).permit(:client_id, :organisation_id, :number, :date_of_issue, :po_number, :discount, :note)
+  	params.require(:invoice).permit(
+      :client_id, 
+      :organisation_id, 
+      :number, 
+      :date_of_issue, 
+      :po_number, 
+      :discount, 
+      :note, 
+      :items_attributes => [:qty])
   end
 end
