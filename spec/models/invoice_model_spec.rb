7a7bfd59b788ec7c899f8f9a1375abbf3a8d1f91
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-05-05 16:02:49 +0100

    Update spec test and remove test for nested attribute

--- spec/models/invoice_model_spec.rb
+++ spec/models/invoice_model_spec.rb
@@ -1,10 +1,6 @@
 	require 'rails_helper'
 
 describe Invoice do 
-	describe 'nested attributes' do 
-		it { should accept_nested_attributes_for :items }
-	end
-
 	describe 'relations' do
 		it { is_expected.to belong_to :organisation }
 		it { is_expected.to belong_to :client }
