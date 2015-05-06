e8d54215d81e2c59be6c2f7378cd9b356e66ccdd
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-05-06 15:21:59 +0100

    Ability to increment index

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,4 +10,11 @@ class Item
 	field :description, type: String
 	field :price, type: String
 	field :total, type: String
+
+	# used by ajax link in _form.html to increment
+	# nested attributes index
+	def self.index_increment
+		@index ||= -1
+		@index += 1
+	end
 end
