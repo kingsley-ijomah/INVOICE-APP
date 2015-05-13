a7f27c36dae536a1a4fd6722da393e309a470f20
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-05-13 07:26:11 +0100

    Adding spec test for invoice feature

--- spec/features/invoice_feature_spec.rb
+++ spec/features/invoice_feature_spec.rb
@@ -7,9 +7,13 @@ feature Invoice do
 		expect(current_path).to eq new_invoice_path
 	end
 
-	scenario 'valid invoice', js: true do
+	scenario 'valid invoice', js: false do
 		client = create(:client)
 		item_kind = create(:item_kind)
+		item_kind_name = item_kind.name
+		date = Time.now
+		item_qty = 3
+		note = 'thank you for your business'
 
 		visit new_invoice_path
 		click_link 'add_line'
@@ -17,17 +21,27 @@ feature Invoice do
 		expect {
 			select client.first_name, from: 'invoice_client_id'
 			fill_in 'invoice_number', with: '11'
-			fill_in 'invoice_date_of_issue', with: Time.now
-			fill_in 'invoice_po_number', with: '1'
+			fill_in 'invoice_date_of_issue', with: date
+			fill_in 'invoice_po_number', with: '88787'
 			fill_in 'invoice_discount', with: '10'
-			fill_in 'invoice_note', with: 'thank you for your business'
+			fill_in 'invoice_note', with: note
 
 			fill_in 'invoice_items_0_qty', with: '3'
-			select item_kind.name, from: 'invoice_items_0_item_kind_id'
+			select item_kind_name, from: 'invoice_items_0_item_kind_id'
 			fill_in 'invoice_items_0_description', with: 'A month of coding'
 			fill_in 'invoice_items_0_price', with: 3000
 
 			click_button 'Save Draft'
 		}.to change(Invoice, :count).by(1)
+
+		expect(page.find('.invoice_no')).to have_content(11)
+		expect(page.find('.invoice_status')).to have_content('draft')
+		expect(page.find('#invoice_number')).to have_content(11)
+		expect(page.find('#invoice_date')).to have_content(date)
+		expect(page.find('#po')).to have_content(88787)
+		expect(page.find('.item_note')).to have_content(note)
+		expect(page.find('.item_qty')).to have_content(item_qty)
+		expect(page.find('.item_kind')).to have_content(item_kind_name)
+		expect(page.find('.line_total')).to have_content(item_kind_name)
 	end
 end
