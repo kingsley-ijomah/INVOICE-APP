6c00fbce726255e8bc3908cb965e9f3644aa0737
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-05-30 23:26:21 +0100

    update company model spec

---
+++ spec/models/company_model_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe Company do
+
+  describe 'validation' do 
+  	it { should validate_presence_of :name }
+  	it { should validate_presence_of :street_1 }
+  	it { should validate_presence_of :city }
+  	it { should validate_presence_of :town }
+  end
+
+  describe 'relations' do
+  	it { is_expected.to have_many(:invoices).with_foreign_key(:company_id) }
+  end
+end
