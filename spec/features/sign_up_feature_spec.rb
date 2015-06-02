b3e720a44aa16b75873c9d5873f044e7c001d790
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-06-02 13:50:06 +0100

    Add logout to background method

--- spec/features/sign_up_feature_spec.rb
+++ spec/features/sign_up_feature_spec.rb
@@ -1,20 +1,23 @@
 require "rails_helper"
 
 feature "User creates account" do
-  
+  background do
+    logout
+  end
+
   scenario "with valid data" do
-  	  visit new_user_registration_path
-      
-      within "#new_user" do
-    		fill_in 'user_name', with: 'kingsley'
-  	  	fill_in 'user_email', with: 'kingsley3@test.com'
-  	  	fill_in 'user_password', with: 'thisisthetestsubmit'
-  	  	fill_in 'user_password_confirmation', with: 'thisisthetestsubmit'
-      end
-      click_button 'Sign up'
-     
-	 	 	expect(current_path).to eq(root_path)
-      expect(page.find('.notice')).to have_content('Welcome! You have signed up successfully.')
+    visit new_user_registration_path
+
+    within "#new_user" do
+      fill_in 'user_name', with: 'kingsley'
+      fill_in 'user_email', with: 'kingsley3@test.com'
+      fill_in 'user_password', with: 'thisisthetestsubmit'
+      fill_in 'user_password_confirmation', with: 'thisisthetestsubmit'
+    end
+    click_button 'Sign up'
+
+    expect(current_path).to eq(root_path)
+    expect(page.find('.notice')).to have_content('Welcome! You have signed up successfully.')
   end
 
   scenario 'click Login in' do 
