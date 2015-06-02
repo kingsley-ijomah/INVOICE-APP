acac0e0ffd439f1a57921fce05804d3afaa67486
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-06-02 13:47:09 +0100

    Fix indentation

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,8 +3,8 @@ FactoryGirl.define do
 
   factory :user do
     name Faker::Name.name
-	email Faker::Internet.email
-	password pass
-	password_confirmation pass
+    email Faker::Internet.email
+    password pass
+    password_confirmation pass
   end
 end
