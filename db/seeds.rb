07295f9054800a9cb50ae21d4a7121cc4a9a0def
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-06-03 06:57:29 +0100

    Add sedding for company

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 ItemKind.create([{ name: 'hours' }, { name: 'service' }, { name: 'product' }, { name: 'days' }, { name: 'deposit' }, { name: 'expenses' }])
+Company.create(name: 'KOCO', street_1: 'Blackwell', street_2: 'Hatton', city: 'Warwickshire', town: 'Warwick', post_code: 'CV35');
