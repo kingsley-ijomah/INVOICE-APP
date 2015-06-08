5b976ea1f157d3a7edb1a8bb1d500fb6d6eb1c30
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-06-08 18:52:15 +0100

    Add total sum up

--- app/assets/javascripts/invoices.js
+++ app/assets/javascripts/invoices.js
@@ -1,13 +1,52 @@
 $(document).on('keyup mouseup', '.qty-input', function() {
-  calc_line_total($(this));
+  program($(this));
 });
 
 
 $(document).on('keyup', '.price-input', function() {
-  calc_line_total($(this));
+  program($(this));
 });
 
+$(document).on('click', '.remove-line', function() {
+  var total = strip_none_int($('#total').text());
+
+  $('#item-list tr').each(function(i, tr) {
+    t = $(tr).find('.line-total').text();
+    total += strip_none_int(t); 
+  });
+
+  $('#total').text(accounting.formatMoney(total));
+});
+
+function program(scope) {
+  calc_line_total(scope);
+  calc_total();
+  calc_balance();
+}
+
+function calc_balance() {
+  var total = strip_none_int($('#total').text());
+  var paid = strip_none_int($('#paid').text());
+  $('#balance').text(accounting.formatMoney(total - paid));
+}
+
+function strip_none_int(string) {
+  return string.replace(/[^\d.]/g, '');
+}
+
+function calc_total() {
+  var total = 0;
+  $('.line-total').each(function() {
+    total += parseInt(strip_none_int($(this).text()));
+  }); 
+
+  accounting.settings.currency.symbol = "£";
+  $('#total').text(accounting.formatMoney(total));
+}
+
+
 function calc_line_total(scope) {
+
   var qty = scope.parents('tr').find('.qty-input').val();
   var price = scope.parents('tr').find('.price-input').val();
   var line_total = qty * price;
@@ -15,3 +54,5 @@ function calc_line_total(scope) {
   accounting.settings.currency.symbol = "£";
   scope.closest('tr').find('.line-total').text(accounting.formatMoney(line_total));
 }
+
+
