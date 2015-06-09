a7dbc4a2af42eba69cf7add757d16bb5a4ee0e32
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-06-09 13:55:24 +0100

    Ability to remove line

--- app/assets/javascripts/invoices.js
+++ app/assets/javascripts/invoices.js
@@ -7,15 +7,10 @@ $(document).on('keyup', '.price-input', function() {
   program($(this));
 });
 
-$(document).on('click', '.remove-line', function() {
-  var total = strip_none_int($('#total').text());
-
-  $('#item-list tr').each(function(i, tr) {
-    t = $(tr).find('.line-total').text();
-    total += strip_none_int(t); 
-  });
-
-  $('#total').text(accounting.formatMoney(total));
+$(document).on("click", '.remove-line', function(e) {
+  $(this).closest('tr').remove();
+  calc_total();
+  calc_balance();
 });
 
 function program(scope) {
@@ -36,17 +31,16 @@ function strip_none_int(string) {
 
 function calc_total() {
   var total = 0;
-  $('.line-total').each(function() {
-    total += parseInt(strip_none_int($(this).text()));
+  $('.line-total').each(function(i, line) {
+    total += parseFloat(strip_none_int($(line).text()));
+    console.log(total);
   }); 
 
   accounting.settings.currency.symbol = "£";
   $('#total').text(accounting.formatMoney(total));
 }
 
-
 function calc_line_total(scope) {
-
   var qty = scope.parents('tr').find('.qty-input').val();
   var price = scope.parents('tr').find('.price-input').val();
   var line_total = qty * price;
