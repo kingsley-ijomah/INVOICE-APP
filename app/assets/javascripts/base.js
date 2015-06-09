129eae9b78e9b03f5dee6971c87ccdb47440d29c
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-06-09 16:52:00 +0100

    Update

--- app/assets/javascripts/base.js
+++ app/assets/javascripts/base.js
@@ -3,9 +3,7 @@
 
 
 //= require jquery/jquery-1.11.1.min
-//= require bootstrap/js/bootstrap.min
 //= require modernizr/modernizr-2.6.2.min
-//= require navgoco/jquery.navgoco.min
 //= require pace/pace.min
 //= require html5shiv/html5shiv.min
 //= require respond/respond.min
