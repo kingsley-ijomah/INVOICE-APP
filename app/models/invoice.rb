f8509b5db1f045b6dcf6b92290f4b19742b811a6
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-06-10 15:24:21 +0100

    Change number type to integer

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -7,7 +7,7 @@ class Invoice
   belongs_to :company
 	has_many :items, autosave: true
 
-	field :number, type: String
+	field :number, type: Integer
 	field :date_of_issue, type: DateTime
 	field :po_number, type: String
 	field :discount, type: Integer
