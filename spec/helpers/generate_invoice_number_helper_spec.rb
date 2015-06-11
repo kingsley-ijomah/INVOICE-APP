f822b9a12ec40f63e4dded35c79f64d6540385a2
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-06-11 14:06:10 +0100

    Add test for generate invoice number

--- spec/helpers/generate_invoice_number_helper_spec.rb
+++ spec/helpers/generate_invoice_number_helper_spec.rb
@@ -4,15 +4,21 @@ describe InvoiceNumberHelper, type: :helper do
   describe '#generate_invoice_number' do
     context 'when no invoice exists in database' do
       it 'returns 00001 as first invoice' do
-        expect(helper.generate_invoice_number).to eq '00001'
+        expect(helper.generate_invoice_number).to eq (helper.invoice_prefix + '1')
       end
     end
 
     context 'when an invoice exists in database' do
       it 'should increment to 00002' do
         create(:invoice, number: 1)
-        expect(helper.generate_invoice_number).to eq '00002'
+        expect(helper.generate_invoice_number).to eq (helper.invoice_prefix + '2')
       end
+    end
+  end
+
+  describe '#invoice_prefix' do
+    it 'it add 0000 prefix to invoice' do
+      expect(helper.invoice_prefix).to eq ('0000')
     end
   end
 end
