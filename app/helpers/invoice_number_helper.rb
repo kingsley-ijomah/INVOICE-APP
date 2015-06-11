736f0225ca4a53c3090d38cdb34441e831ca5c81
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-06-11 14:03:21 +0100

    Add prefix method

--- app/helpers/invoice_number_helper.rb
+++ app/helpers/invoice_number_helper.rb
@@ -1,10 +1,14 @@
 module InvoiceNumberHelper
+  def invoice_prefix
+    '0000'
+  end
+
   def generate_invoice_number
     if Invoice.empty?
-      '00001'
+      invoice_prefix + '1'
     else 
       n = Invoice.max(:number)
-      '0000' + (n += 1).to_s
+      invoice_prefix + (n += 1).to_s
     end
   end
 end
