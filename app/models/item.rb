2fde5d8f348edda990173307066473384d07fc18
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-06-13 16:11:11 +0100

    Add float type

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,8 +7,12 @@ class Item
 
 	field :qty, type: Integer
 	field :description, type: String
-	field :price, type: String
-	field :total, type: String
+	field :price, type: Float
+	field :total, type: Float
+
+  before_save do
+    self.total = self.price * self.qty
+  end
 
 	# used by ajax link in _form.html to increment
 	# nested attributes index
