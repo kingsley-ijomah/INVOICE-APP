ff1bd3fd08fc1051102496b22b13d3204c0c2b37
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-06-14 07:30:47 +0100

    Model test for totals

--- spec/models/invoice_model_spec.rb
+++ spec/models/invoice_model_spec.rb
@@ -8,12 +8,21 @@ describe Invoice do
 	end
 
 	describe 'fields' do
-		it { is_expected.to have_fields(:number, :date_of_issue, :po_number, :discount, :note, :terms)}
+		it { is_expected.to have_fields(:number, :date_of_issue, :po_number, :discount, :note, :terms, :total)}
 	end
 
 	describe 'validation' do 
   	it { should validate_presence_of :number }
   	it { should validate_presence_of :date_of_issue }
   	it { should validate_presence_of :terms }
+  end
+
+  describe '#total' do
+    it 'calculate multiple row totals' do
+      item_1 = create(:item, qty: 1, price: 2.50)
+      item_2 = create(:item, qty: 1, price: 2.75, invoice_id: item_1.invoice.id)
+     
+      expect(item_2.invoice.total).to eq (item_1.total + item_2.total) 
+    end
   end
 end
