539736fa9af3f3d71df42600b7647f6a12b64a4a
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-06-15 07:14:35 +0100

    Update model spec

--- spec/models/invoice_model_spec.rb
+++ spec/models/invoice_model_spec.rb
@@ -17,12 +17,36 @@ describe Invoice do
   	it { should validate_presence_of :terms }
   end
 
-  describe '#total' do
+  describe '#calculate_items_totals' do
     it 'calculate multiple row totals' do
-      item_1 = create(:item, qty: 1, price: 2.50)
-      item_2 = create(:item, qty: 1, price: 2.75, invoice_id: item_1.invoice.id)
-     
-      expect(item_2.invoice.total).to eq (item_1.total + item_2.total) 
+      @invoice = create(:invoice)
+      item_1 = create(:item, qty: 1, price: 2.50, invoice_id: @invoice.id)
+      item_2 = create(:item, qty: 1, price: 2.75, invoice_id: @invoice.id)
+
+      expect(@invoice.reload.calculate_items_totals).to eq (item_1.total + item_2.total)
     end
   end
+
+  describe '#update_total' do
+    it 'updates invoice total when you add new item' do
+      @invoice = create(:invoice)
+      item_1 = create(:item, qty: 1, price: 2.50, invoice_id: @invoice.id)
+      item_2 = create(:item, qty: 1, price: 2.75, invoice_id: @invoice.id)
+
+      expect(@invoice.reload.total).to eq (item_1.total + item_2.total)
+    end
+  end
+
+  describe '#invoice_totals' do
+    it 'calculates the total for all invoices' do
+      invoice_1 = create(:invoice)
+      invoice_2 = create(:invoice)
+  
+      2.times { create(:item, qty: 1, price: 250, invoice_id: invoice_1.id) }
+      2.times { create(:item, qty: 1, price: 250, invoice_id: invoice_2.id) }
+
+      expect(Invoice.total).to eq 1000.0
+    end
+  end
+
 end
