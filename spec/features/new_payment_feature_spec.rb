f9110df982980dc39d558d5101cd8b1ec5531513
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-06-16 21:25:12 +0100

    Add feature for payment

---
+++ spec/features/new_payment_feature_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+feature Payment do
+  scenario 'Successfully enter payment' do
+    invoice = create(:invoice)
+    5.times { create(:item, invoice_id: invoice.id) }
+
+    visit invoice_path(invoice.id)
+
+    click_link 'Enter Payment'
+
+    # expect {
+      fill_in 'payment_amount', with: 600.50
+      fill_in 'payment_date', with: DateTime.now.beginning_of_day
+      fill_in 'payment_note', with: Faker::Lorem.sentence
+      click_button 'Save Payment'
+    # }.to change(Payment, :count).by(1)
+    save_and_open_page
+    
+    expect(page.find('#panel-title')).to have_content('#0000' + invoice.number.to_s)
+    expect(page.find('#amount')).to have_content(invoice.total)
+  end
+end
