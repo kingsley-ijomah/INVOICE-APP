14d5aa9f7ee826821184c9e1a62c0a17d4a7828a
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-06-16 21:23:11 +0100

    Add payments controller

---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,22 @@
+class PaymentsController < ApplicationController
+  layout 'plain', :only => [:new]
+
+  def new
+    @invoice = Invoice.find(params[:invoice_id])
+    @payment = Payment.new(invoice: @invoice)
+  end
+
+  def create
+    @payment = Payment.new(payments_params)
+    if @payment.save
+      redirect_to invoices_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def payments_params
+    params.require(:payment).permit(:amount, :date, :note).merge(invoice_id: params[:invoice_id])
+  end    
+end
