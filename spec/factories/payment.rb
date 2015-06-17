7ac1c718e282a83eaaf8caf84ec89cbe13c7b555
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-06-17 07:14:15 +0100

    Update factory

--- spec/factories/payment.rb
+++ spec/factories/payment.rb
@@ -1,9 +1,10 @@
 FactoryGirl.define do
 
   factory :payment do
-    amount Kernel::rand(500..3000)
-    date DateTime.now.beginning_of_day
+    amount Kernel::rand(500..3000).to_s
+    date '2015-06-17T00:00:00+01:00'
 		note Faker::Lorem.sentence
+    association :invoice
   end
 end
 
