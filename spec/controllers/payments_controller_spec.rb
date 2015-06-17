3c7152dcebd7b27d9d964c5ce5ee9453976715ea
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-06-17 07:12:54 +0100

    Update valid and invalid posts

--- spec/controllers/payments_controller_spec.rb
+++ spec/controllers/payments_controller_spec.rb
@@ -7,7 +7,7 @@ describe PaymentsController do
       payment = double('payment')
       allow(Invoice).to receive(:find).with(invoice.id).and_return(invoice)      
       allow(Payment).to receive(:new).with(invoice: invoice).and_return(payment)
-      
+
       get 'new', invoice_id: invoice.id
 
       expect(Invoice).to have_received(:find).with(invoice.id)
@@ -23,30 +23,30 @@ describe PaymentsController do
       invoice = double('invoice', id: '1')
       payment = double('payment')
       attributes = attributes_for(:payment)
-      
+
       allow(Payment).to receive(:new).with(attributes.merge(invoice_id: invoice.id)).and_return(payment)
       allow(payment).to receive(:save).and_return(true)
-      
+
       post 'create', invoice_id: invoice.id, payment: attributes
 
       expect(Payment).to have_received(:new).with(attributes.merge(invoice_id: invoice.id))
       expect(payment).to have_received(:save)
-      expect(response).to redirect_to invoice_url(invoice)
+      expect(response).to redirect_to invoices_url
     end
 
-    # it 'invalid post' do
-    #   invoice = double('invoice', id: '1')
-    #   payment = double('payment')
-    #   attributes = attributes_for(:invoice)
+    it 'invalid post' do
+      invoice = double('invoice', id: '1')
+      payment = double('payment')
+      attributes = attributes_for(:payment)
 
-    #   allow(Payment).to receive(:new).with(attributes).and_return(payment)
-    #   allow(payment).to receive(:save).and_return(false)
-      
-    #   post 'create', invoice_id: invoice.id, payment: attributes
+      allow(Payment).to receive(:new).with(attributes.merge(invoice_id: invoice.id)).and_return(payment)
+      allow(payment).to receive(:save).and_return(false)
 
-    #   expect(Payment).to have_received(:new).with(attributes)
-    #   expect(payment).to have_received(:save)
-    #   expect(response).to render_template 'new'
-    # end
+      post 'create', invoice_id: invoice.id, payment: attributes
+
+      expect(Payment).to have_received(:new).with(attributes.merge(invoice_id: invoice.id))
+      expect(payment).to have_received(:save)
+      expect(response).to render_template 'new'
+    end
   end
 end
