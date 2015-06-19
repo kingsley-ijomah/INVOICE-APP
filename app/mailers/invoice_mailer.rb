88863ee5104edfb1200a175b600d623851aacb0e
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-06-19 17:17:59 +0100

    Generate mailer files

---
+++ app/mailers/invoice_mailer.rb
@@ -0,0 +1,12 @@
+class InvoiceMailer < ApplicationMailer
+  include ApplicationHelper
+
+  default from: 'kingsley.ijomah@gmail.com'
+
+  def send_invoice_email(client:, invoice:)
+    @client = client
+    @invoice = invoice
+
+    mail(to: @client.email, subject: 'Invoice from KOCO')
+  end
+end
