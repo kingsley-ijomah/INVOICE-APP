dbfb373fc19a02f26b646b88be5f995c211b6454
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-06-20 07:49:01 +0100

    Add ability to send mail and display notice

--- app/controllers/send_invoices_controller.rb
+++ app/controllers/send_invoices_controller.rb
@@ -6,5 +6,9 @@ class SendInvoicesController < ApplicationController
   end 
 
   def create
+    @invoice = Invoice.find params[:invoice_id]
+    @client = @invoice.client
+    InvoiceMailer.send_invoice_email(client: @client, invoice: @invoice).deliver_now
+    redirect_to @invoice, notice: 'You have successfully sent invoice #0000' + @invoice.number.to_s + ' to ' + @client.organisation.name 
   end
 end
