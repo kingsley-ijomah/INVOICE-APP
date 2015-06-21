c9446ea9bc12900d12070772558a12b79a9dae1f
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-06-21 14:40:46 +0100

    Add helpers

--- app/mailers/invoice_mailer.rb
+++ app/mailers/invoice_mailer.rb
@@ -1,5 +1,6 @@
 class InvoiceMailer < ApplicationMailer
   default from: 'kingsley.ijomah@gmail.com'
+  helper :application, :invoice_number
 
   def send_invoice_email(client:, invoice:)
     @client = client
