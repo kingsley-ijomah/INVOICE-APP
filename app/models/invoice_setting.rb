37e634d9c28fc2f942766241ef088cf00cba7d9f
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-09-07 16:22:29 +0100

    Adding invoice settings fields

--- app/models/invoice_setting.rb
+++ app/models/invoice_setting.rb
@@ -1,2 +1,7 @@
-class InvoiceSetting < ActiveRecord::Base
+class InvoiceSetting
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :name, type: String
+  field :value, type: String
 end
