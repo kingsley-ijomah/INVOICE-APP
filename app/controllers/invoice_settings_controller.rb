d7a512dfa718aadaf95ca5ca173f08e46af27649
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-09-09 14:54:29 +0100

    Adding create method

--- app/controllers/invoice_settings_controller.rb
+++ app/controllers/invoice_settings_controller.rb
@@ -4,4 +4,13 @@ class InvoiceSettingsController < ApplicationController
   def new
     @invoice_settings = InvoiceSetting.new
   end
+
+  def create
+    @invoice_settings = InvoiceSetting.new(invoice_settings_params)
+    InvoiceSetting.save(@invoice_settings)
+  end
+
+  def invoice_settings_params
+    params.require(:invoice_settings).permit(:terms, :notes)
+  end
 end
