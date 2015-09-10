d5590b59d127544595751924132665ad7fba04c6
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-09-10 20:16:04 +0100

    Updating controller

--- app/controllers/invoice_settings_controller.rb
+++ app/controllers/invoice_settings_controller.rb
@@ -1,16 +1,26 @@
 class InvoiceSettingsController < ApplicationController
-	layout 'plain', :only => [:new]
+	layout 'plain'
 
   def new
-    @invoice_settings = InvoiceSetting.new
+    @invoice_setting = InvoiceSetting.new
   end
 
   def create
-    @invoice_settings = InvoiceSetting.new(invoice_settings_params)
-    InvoiceSetting.save(@invoice_settings)
+    @invoice_setting = InvoiceSetting.new(invoice_settings_params)
+    respond_to do |format|
+      if @invoice_setting.save
+        format.js
+        format.html { redirect_to(@invoice_setting, :notice => 'Article was successfully created.') }
+        # format.xml  { render :xml => @invoice_setting, :status => :created, :location => @invoice_setting }
+      else
+        # format.html { render :action => "new" }
+        # format.xml  { render :xml => @invoice_setting.errors, :status => :unprocessable_entity }
+        format.js
+      end
+    end
   end
 
   def invoice_settings_params
-    params.require(:invoice_settings).permit(:terms, :notes)
+    params.require(:invoice_setting).permit(:terms, :notes)
   end
 end
