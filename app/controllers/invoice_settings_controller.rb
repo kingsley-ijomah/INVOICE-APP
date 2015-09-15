0a4b395f10578f26c99bbaf3d02ba2db9b90504d
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-09-15 15:18:06 +0100

    Prevent multiple records being saved

--- app/controllers/invoice_settings_controller.rb
+++ app/controllers/invoice_settings_controller.rb
@@ -1,5 +1,5 @@
 class InvoiceSettingsController < ApplicationController
-	layout 'plain'
+  layout 'plain'
 
   def new
     @invoice_setting = InvoiceSetting.new
@@ -8,14 +8,19 @@ class InvoiceSettingsController < ApplicationController
   def create
     @invoice_setting = InvoiceSetting.new(invoice_settings_params)
     respond_to do |format|
-      if @invoice_setting.save
+      if !InvoiceSetting.first.nil?
+        InvoiceSetting.first.update(invoice_settings_params)
         format.js
-        # format.html { redirect_to(@invoice_setting, :notice => 'Article was successfully created.') }
-        # format.xml  { render :xml => @invoice_setting, :status => :created, :location => @invoice_setting }
-      else
-        # format.html { render :action => "new" }
-        # format.xml  { render :xml => @invoice_setting.errors, :status => :unprocessable_entity }
-        format.js
+      else 
+        if @invoice_setting.save
+          format.js
+          # format.html { redirect_to(@invoice_setting, :notice => 'Article was successfully created.') }
+          # format.xml  { render :xml => @invoice_setting, :status => :created, :location => @invoice_setting }
+        else
+          # format.html { render :action => "new" }
+          # format.xml  { render :xml => @invoice_setting.errors, :status => :unprocessable_entity }
+          format.js
+        end
       end
     end
   end
